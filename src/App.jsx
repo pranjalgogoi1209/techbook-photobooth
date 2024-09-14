ac41f2642cfcb825be20c287bc1b480bd1d0655d
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-09-14 12:03:07 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import CameraPage from "./pages/cameraPage/CameraPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./homePage.scss";
 
 import Model3d from "../../components/model3d/Model3d";
+import Header from "../../components/header/Header";
 
 export default function HomePage() {
   return (
-    <div>
-      this is home page
-      <Model3d />
+    <div className="HomePage flex-col-center">
+      <Header />
+      {/* <Model3d /> */}
+      <div className="start_button">
+        <button className="flex-row-center">
+          <img src="/start.png" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
