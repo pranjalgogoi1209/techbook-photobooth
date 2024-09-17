de4277952dc8f70e8c70cf601017e5f084ee32f2
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-09-17 11:00:03 +0530

    added croping

--- src/utils/getScreenshot.js
+++ src/utils/getScreenshot.js
@@ -23,41 +23,66 @@ const getScreenshot = ({ element, type }, callback) => {
   }
 
   // Capture the screenshot using html2canvas
-  html2canvas(element, {
-    useCORS: true,
+  if(type=="withFrame"){
+    html2canvas(element,{
+      useCORS: true,
     scale: 5,
-  }).then((canvas) => {
-    if (type === "withFrame") {
-      // Create a new canvas for cropping
-      const croppedCanvas = document.createElement("canvas");
-      const ctx = croppedCanvas.getContext("2d");
-
-      // Set the new canvas dimensions (same width, but 50px less in height)
-      croppedCanvas.width = canvas.width;
-      croppedCanvas.height = canvas.height - 50; // Crop 50px from the bottom
-
-      // Draw the original canvas onto the cropped canvas (except the bottom 50px)
-      ctx.drawImage(
-        canvas,
-        0,
-        0,
-        canvas.width,
-        canvas.height - 50,
-        0,
-        0,
-        canvas.width,
-        canvas.height - 50
-      );
-
-      // Convert the cropped canvas to base64 and pass it to the callback
-      const base64Image = croppedCanvas.toDataURL("image/png");
-      callback(base64Image);
-    } else {
-      // For other types, just return the full canvas screenshot
+    x: 0,
+    y: 0,
+    width: element.offsetWidth,
+    height: element.offsetHeight - 100, 
+    }).then((canvas)=>{
       const base64Image = canvas.toDataURL("image/png");
       callback(base64Image);
-    }
-  });
+    })
+  }else{
+    html2canvas(element,{
+      useCORS:true,
+      scale:5
+    }).then((canvas)=>{
+      const base64Image = canvas.toDataURL("image/png");
+      callback(base64Image);
+    })
+  }
+  // html2canvas(element, {
+  //   useCORS: true,
+  //   scale: 5,
+  //   x: 0,
+  //   y: 0,
+  //   width: element.offsetWidth,
+  //   height: element.offsetHeight - 200, 
+  // }).then((canvas) => {
+  //   if (type === "withFrame") {
+  //     // Create a new canvas for cropping
+  //     const croppedCanvas = document.createElement("canvas");
+  //     const ctx = croppedCanvas.getContext("2d");
+
+  //     // Set the new canvas dimensions (same width, but 50px less in height)
+  //     croppedCanvas.width = canvas.width;
+  //     croppedCanvas.height = canvas.height - 50; // Crop 50px from the bottom
+
+  //     // Draw the original canvas onto the cropped canvas (except the bottom 50px)
+  //     ctx.drawImage(
+  //       canvas,
+  //       0,
+  //       0,
+  //       canvas.width,
+  //       canvas.height - 50,
+  //       0,
+  //       0,
+  //       canvas.width,
+  //       canvas.height - 50
+  //     );
+
+  //     // Convert the cropped canvas to base64 and pass it to the callback
+  //     const base64Image = canvas.toDataURL("image/png");
+  //     callback(base64Image);
+  //   } else {
+  //     // For other types, just return the full canvas screenshot
+  //     const base64Image = canvas.toDataURL("image/png");
+  //     callback(base64Image);
+  //   }
+  // });
 };
 
 export default getScreenshot;
