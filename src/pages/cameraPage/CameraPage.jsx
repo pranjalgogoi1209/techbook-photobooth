3991757e9252d0e020c2d54b9f4db2ed786368da
unknown <gogoipranjal2022@gmail.com>
2026-08-25 15:37:49 +0530

    update

--- src/pages/cameraPage/CameraPage.jsx
+++ src/pages/cameraPage/CameraPage.jsx
@@ -903,7 +903,7 @@ export default function CameraPage({
                     inset: 0,
                     width: "100%",
                     height: "100%",
-                    objectFit: "contain",
+                    objectFit: "cover",
                     objectPosition: "center",
                     zIndex: 1,
                     display: "block",
